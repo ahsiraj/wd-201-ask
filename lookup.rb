afecd9f72b8c7a2708851a900451c28aeb3d0629
ask@ceconline.edu <ask@ceconline.edu>
2021-09-04 00:43:04 +0530

    DNS work finished. Extra entries in zone file

--- lookup.rb
+++ lookup.rb
@@ -20,20 +20,42 @@ dns_raw = File.readlines("zone")
 
 # ..
 # ..
-# FILL YOUR CODE HERE
+# FILL YOUR CODE HERE. Filled!(ask - 4/9/21)
 # ..
 # ..
-def parse_dns(dns_raw)
-  dns_raw.each { |line|
-    if line.strip != "" && !line.start_with?("#")
-      puts line
+#====================================================================================================
+def parse_dns(dns_raw) # dns entries from the file are structured here
+  dns_array = []
+  dns_records = {}
+  dns_raw.each { |line| # each line is processed
+    if line.strip != "" && !line.strip.start_with?("#") # empty, only space, and comment lines omitted
+      #  puts line
+      dns_array.push(line.split(",").map(&:strip)) # pushed the dns entries from each line after removing space if any
     end
   }
+  #puts dns_array
+  #puts "---------------"
+  dns_array.each { |type, source, destination| dns_records[source.to_sym] = [type, destination] } #dns source is taken as the key
+  #puts dns_records
+  dns_records
 end
 
-def resolve(dns_records, lookup_chain, domain)
+def resolve(dns_records, lookup_chain, domain) #dns resolution
+  matched_dns = dns_records[domain.to_sym] # entry in the table
+  if matched_dns == nil #no match
+    lookup_chain.push("Domain name could not be resolved. Recheck for typos!")
+  elsif matched_dns[0] == "A" #IP address match
+    lookup_chain.push(matched_dns[1])
+  elsif matched_dns[0] == "CNAME"
+    lookup_chain.push(matched_dns[1])
+    resolve(dns_records, lookup_chain, matched_dns[1]) # recrsive call for CNAME type
+  else # entry is there, but type mismatch
+    lookup_chain.push(matched_dns[1])
+    lookup_chain.push("Could not finish the resolve chain. Something is wrong. Internal entry type mismatch.")
+  end
 end
 
+#=======================================================================================================
 # To complete the assignment, implement `parse_dns` and `resolve`.
 # Remember to implement them above this line since in Ruby
 # you can invoke a function only after it is defined.
