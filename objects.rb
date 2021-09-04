049ae5cca078dd27326c902434b02fbb8f0c07a2
ask@ceconline.edu <ask@ceconline.edu>
2021-09-04 14:27:08 +0530

    Inheritance example added

--- objects.rb
+++ objects.rb
@@ -48,6 +48,9 @@ bava = Person.new("Ahammed", "Siraj")
 puts bava.full_name_greeting
 
 #Another example--- Tell don't Ask!
+puts "TdA  !!!"
+puts "=============="
+
 class Course
   def initialize(name, students)
     @name = name
@@ -64,6 +67,8 @@ c.add_student("X")
 c.add_student("Y")
 
 #More ================= Object composition
+puts "Objects Composition"
+
 class Course
   def initialize(name, students)
     @name = name
@@ -94,3 +99,46 @@ puts saas.is_student_enrolled?(800)   # false
 saas.enroll(Student.new(800, "Ned"))
 puts saas.is_student_enrolled?(800)   # true
 puts saas.get_student(800).name       # Ned
+
+puts "==============Inheritance===================="
+
+class Department
+  def initialize(name, classrooms_count)
+    @name = name
+    @classrooms_count = classrooms_count
+    @teachers = []
+  end
+
+  def add_teacher(teacher)
+    @teachers.push(teacher)
+  end
+
+  def rooms_info
+    "Classrooms count: #{@classrooms_count}"
+  end
+
+  def teachers_info
+    "Number of teachers: #{@teachers.length}"
+  end
+
+  def department_info
+    "Department: #{@name}. #{rooms_info} #{teachers_info}"
+  end
+end
+
+class ComputerDepartment < Department
+  def initialize(name, classrooms_count, labs_count, conf_count)
+    super(name, classrooms_count)
+    @labs_count = labs_count
+    @conf_count = conf_count
+  end
+
+  def rooms_info
+    "Rooms count -- classrooms: #{@classrooms_count}, labs: #{@labs_count}, conference rooms: #{@conf_count}"
+  end
+end
+
+c = ComputerDepartment.new("Computer Science & Engineering", 3, 2, 1)
+c.add_teacher("Teacher 1")
+c.add_teacher("Teacher 2")
+puts c.department_info
