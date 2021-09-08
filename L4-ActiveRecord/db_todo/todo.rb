0e36815f32184cf498f8625f545c72506025971e
ask@ceconline.edu <ask@ceconline.edu>
2021-09-08 23:13:40 +0530

    L4 assignment 1 complted - fine tuning remain

--- L4-ActiveRecord/db_todo/todo.rb
+++ L4-ActiveRecord/db_todo/todo.rb
@@ -58,9 +58,16 @@ class Todo < ActiveRecord::Base
     puts "\n\n"
   end
 
-  def self.add_task(todo_text, due_in_days)
+  def self.add_task(todo)
+    todo_text = todo[:todo_text]
+    due_date = Date.today + todo[:due_in_days]
+    create!(todo_text: todo_text, due_date: due_date, completed: false)
   end
 
-  def mark_as_complete
+  def self.mark_as_complete(todo_id)
+    todo_for_completion = find(todo_id)
+    todo_for_completion.completed = true
+    todo_for_completion.save
+    return todo_for_completion
   end
 end
